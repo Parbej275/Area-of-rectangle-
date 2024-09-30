dd827ee342b4f820f1a654aa90ff19b51748b6ea
Suman Shah <NCE075BEL012@nce.edu.np>
2024-09-30 14:06:11 +0545

    Update area.java

--- area of rectangle/src/com/parbej1/area.java
+++ area of rectangle/src/com/parbej1/area.java
@@ -1,27 +1,36 @@
 package com.parbej1;
 import java.util.Scanner;
 
-public class area {
+public class Area {
     int length;
     int width;
 
+    
     int area(int length, int width) {
         return length * width;
     }
 
-
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+
+        
         System.out.println("Enter the length of the rectangle: ");
         int length = scanner.nextInt();
+
         System.out.println("Enter the width of the rectangle: ");
         int width = scanner.nextInt();
+
         
-        area suman = new area();
-        int rectanglearea = suman.area(length, width);
-        System.out.println("The area of rectangle is " + rectangleArea);
+        Area suman = new Area();
 
+        
+        int rectangleArea = suman.area(length, width);
+
+        
+        System.out.println("The area of the rectangle is: " + rectangleArea);
+
+        
         scanner.close();
-
     }
 }
